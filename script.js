bf4b4514cae337774079f56af064fc7fd9f78e52
Azamat Gaparov <agaparovkg@gmail.com>
2023-11-30 19:19:29 -0800

    new changes

--- script.js
+++ script.js
@@ -21,6 +21,8 @@ let transactions = [];
 //   }
 // ]
 
+// [-200, 900, ...]
+
 // Add transaction
 function addTransaction(e) {
   e.preventDefault();
@@ -37,7 +39,67 @@ function addTransaction(e) {
       text: newTransactionText.value,
       amount: +newTransactionAmount.value,
     };
+
+    transactions.push(transaction);
+
+    updateValues();
+
+    addTransactionToHistory(transaction);
+
+    newTransactionText.value = "";
+    newTransactionAmount.value = "";
   }
+}
+
+// Add each transaction to history list
+function addTransactionToHistory(transaction) {
+  const { text, amount } = transaction;
+  //   {
+  //     id: 2,
+  //     text: "Cash",
+  //     amount: 900
+  //   }
+
+  const item = document.createElement("li");
+  item.classList.add(amount > 0 ? "plus" : "minus");
+
+  item.innerHTML = `${text} <span>-$${Math.abs(
+    amount
+  )}</span><button class="delete-btn">x</button>`;
+
+  historyList.appendChild(item);
+  // <li class="minus">
+  //   Cash <span>-$400</span><button class="delete-btn">x</button>
+  // </li>
+}
+
+// Update balance, income and expense
+function updateValues() {
+  const amounts = transactions.map((transaction) => transaction.amount);
+  // [-200, 900, ...]
+
+  // const total = amounts.reduce((sum, number) => {
+  //   return sum + number
+  // }, 0)
+
+  const total = amounts.reduce((sum, number) => sum + number, 0).toFixed(2);
+
+  const income = amounts
+    .filter((number) => number > 0) // [300, 900, ...]
+    .reduce((sum, num) => sum + num, 0) // 1200
+    .toFixed(2); // 1200.00
+
+  const expense =
+    amounts
+      .filter((number) => number < 0)
+      .reduce((sum, number) => sum + number, 0)
+      .toFixed(2) * -1;
+
+  balanceAmount.innerText = `$${total}`; // "balance is "+total
+  moneyPlusAmount.innerText = `$${income}`;
+  moneyMinusAmount.innerText = `$${expense}`;
+  // moneyMinusAmount.innerText = `$${-expense}`;
+  // moneyMinusAmount.innerText = `$${Math.abs(expense)}`;
 }
 
 // Generate Random ID function
